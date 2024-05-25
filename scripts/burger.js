2744f069a33c3a03c8d9b453cf4a57040507afef
Bogdans Vasilecs <bvasilecs@gmail.com>
2024-05-25 13:55:49 +0300

    burger fix

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,7 +1,6 @@
 const BURGER_OPENED_CLASSNAME = 'burger_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
 
-const bodyNode = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
